79b23c2c8d7d1f24a5810ee41d5658a40b7fb2ee
mohamed <mohamed.benzouine@gmail.com>
2024-01-22 17:05:27 +0100

    Securing REST API's with Database Authentification

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.springboot.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,9 +20,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    private UserDetailsService userDetailsService;
+
+
+    public SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+
+
     @Bean
     public static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    // Database Authentication with AuthenticationManager
+    // AuthenticationManager will use  userDetailsService &  passwordEncoder
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -35,19 +52,19 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService (){
-        UserDetails mohamed = User.builder()
-                .username("mohamed")
-                .password(passwordEncoder().encode("mohamed"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(mohamed, admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService (){
+//        UserDetails mohamed = User.builder()
+//                .username("mohamed")
+//                .password(passwordEncoder().encode("mohamed"))
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(mohamed, admin);
+//    }
 }
